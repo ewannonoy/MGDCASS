326c8ba9127a694af06f91b631e8acbca433612a
Christian Joie B. Harder <Christian Joie B. Harder>
2016-06-07 23:35:31 +0800

    ang main.ts diey!

--- src/main/resources/static/app/main.component.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
-import { HTTP_PROVIDERS } from '@angular/http';
-
-bootstrap(AppComponent, [
-    ROUTER_PROVIDERS,
-    HTTP_PROVIDERS,
-]);
