64a916f97d81998726672c41c4acc6d2d0a7f0e2
Eduardo Esternon <ewannonoy@gmail.com>
2016-06-12 17:30:38 +0800

    Added some entities

---
+++ src/main/java/com/phc/das/entity/Appointment.java
@@ -0,0 +1,32 @@
+package com.phc.das.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Appointment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    // TODO Generated value
+    private String appointmentNo;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String remarks;
+    private String encodedBy;
+
+    @ManyToOne
+    private Branch branch;
+
+}
---
+++ src/main/java/com/phc/das/entity/Branch.java
@@ -0,0 +1,19 @@
+package com.phc.das.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Branch {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String branchName;
+    private String address;
+
+}
