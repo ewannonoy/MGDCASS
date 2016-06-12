9a3aa7c43ee85013db1483b3fae318f19d056770
Christian Joie B. Harder <Christian Joie B. Harder>
2016-06-12 14:42:00 +0800

    fe structure pt2

--- src/main/resources/static/app/app.component.ts
+++ src/main/resources/static/app/app.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import { RouteConfig, RouterOutlet } from '@angular/router-deprecated';
+//Project Components
+import { DashboardComponent } from './layout/dashboard.component'; //location is temporary
+import { NavbarTopComponent } from './layout/navbar-top.component';
 
+@RouteConfig([
+    { path: '/', name: 'Dashboard', component: DashboardComponent, useAsDefault:true},
+    { path: '/*other', name:'Other', redirectTo: ['Dashboard']},
+])
 @Component({
     selector: 'my-app',
-    template: '<h1>Hon is Life!</h1>'
+    template: `
+        <navbar-top></navbar-top>
+        <div class="container" style="margin-top:70px">
+            <router-outlet></router-outlet>
+        </div>
+    `,
+    directives: [NavbarTopComponent, RouterOutlet]
 })
 export class AppComponent { }
