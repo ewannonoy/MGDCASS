af4bd127d94d5428fb3ce7b51f2c5ffc49dd50a7
Christian Joie B. Harder <Christian Joie B. Harder>
2016-06-15 21:25:31 +0800

    transfer to desktop

---
+++ src/main/resources/static/app/users/user.ts
@@ -0,0 +1,24 @@
+export class User{
+    id: number;
+    username: string;
+    password: string;
+}
+
+// export class IAR{
+//     id: number;
+//     IAR_no: string;
+//     category: string;
+//     sub_category: string;
+//     description: string;
+//     purchase_order_no: string;
+//     purchase_order_date: Date;
+//     invoice_no: string;
+//     invoice_date: Date;
+//     supplier: string;
+//     quantity: number;
+//     unit: string;
+//     amount: number;
+//     acquired_date: Date;
+//     remarks: string;
+//     encoder: number;
+// }
